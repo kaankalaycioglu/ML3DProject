e192ac3400e707da83bae1032478463797e27d57
sebastian <sebastian.schreck@tum.de>
2023-07-29 10:32:20 +0200

    added voxel_fsa file

--- pcdet/models/backbones_3d/cfe/__init__.py
+++ pcdet/models/backbones_3d/cfe/__init__.py
@@ -2,6 +2,7 @@ from .voxel_dsa import VoxelContext3D_dsa
 
 
 __all__ = {
-    'VoxelContext3D_dsa': VoxelContext3D_dsa
+    'VoxelContext3D_dsa': VoxelContext3D_dsa,
+    'VoxelContext3D_fsa': VoxelContext3D_fsa,
 }
 
